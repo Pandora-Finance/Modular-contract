603f5aee94bf0e86126c8b41bf8b960993a66333
chanchaldelson@gmail.com <chanchaldelson@gmail.com>
2021-12-07 13:43:37 +0530

    Price issue resolution

--- contracts/Libraries/LibMeta.sol
+++ contracts/Libraries/LibMeta.sol
@@ -21,6 +21,5 @@ library LibMeta {
         token.previousOwner = token.currentOwner;
         token.currentOwner = _to;
         token.numberOfTransfers += 1;
-        token.price = msg.value;
     } 
 }
