3dfaf52d5af29036cd621200ea02977db7a3ed80
Parva Jain <parv@Parvas-MacBook-Air.local>
2022-02-22 13:36:26 +0530

    removed trasfer w call

--- contracts/NFTFactoryContract.sol
+++ contracts/NFTFactoryContract.sol
@@ -12,108 +12,114 @@ import "./PNDC_ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
 
 contract NFTFactoryContract is
-    OwnableUpgradeable,
-    ReentrancyGuardUpgradeable,
-    ERC721HolderUpgradeable,
-    NFTV1Storage
+  OwnableUpgradeable,
+  ReentrancyGuardUpgradeable,
+  ERC721HolderUpgradeable,
+  NFTV1Storage
 {
-    using Counters for Counters.Counter;
+  using Counters for Counters.Counter;
 
-    function initialize() initializer public {
-        OwnableUpgradeable.__Ownable_init();
-        ReentrancyGuardUpgradeable.__ReentrancyGuard_init();
-        ERC721HolderUpgradeable.__ERC721Holder_init();
+  function initialize() public initializer {
+    OwnableUpgradeable.__Ownable_init();
+    ReentrancyGuardUpgradeable.__ReentrancyGuard_init();
+    ERC721HolderUpgradeable.__ERC721Holder_init();
+  }
+
+  event TokenMetaReturn(LibMeta.TokenMeta data, uint256 id);
+
+  modifier onlyOwnerOfToken(address _collectionAddress, uint256 _tokenId) {
+    require(msg.sender == ERC721(_collectionAddress).ownerOf(_tokenId));
+    _;
+  }
+
+  // Change in BuyNFT LibMeta Function
+
+  function BuyNFT(uint256 _saleId) public payable nonReentrant {
+    LibMeta.TokenMeta memory meta = _tokenMeta[_saleId];
+
+    LibShare.Share[] memory royalties;
+
+    if (_tokenMeta[_saleId].collectionAddress == PNDCAddress) {
+      royalties = PNDC_ERC721(PNDCAddress).getRoyalties(
+        _tokenMeta[_saleId].tokenId
+      );
+    } else {
+      royalties = TokenERC721(_tokenMeta[_saleId].collectionAddress)
+        .getRoyalties(_tokenMeta[_saleId].tokenId);
     }
 
-    event TokenMetaReturn(LibMeta.TokenMeta data, uint256 id);
+    require(meta.status);
+    require(msg.sender != address(0) && msg.sender != meta.currentOwner);
+    require(!meta.bidSale);
+    require(msg.value >= meta.price);
 
-    modifier onlyOwnerOfToken(address _collectionAddress, uint256 _tokenId) {
-        require(msg.sender == ERC721(_collectionAddress).ownerOf(_tokenId));
-        _;
+    LibMeta.TokenMeta memory tok = LibMeta.transfer(
+      _tokenMeta[_saleId],
+      msg.sender
+    );
+    _tokenMeta[_saleId] = tok;
+
+    uint256 sum = msg.value;
+    uint256 val = msg.value;
+    uint256 fee = msg.value / 100;
+
+    for (uint256 i = 0; i < royalties.length; i++) {
+      uint256 amount = (royalties[i].value * val) / 10000;
+      address payable receiver = royalties[i].account;
+      receiver.call{ value: amount }("");
+      sum = sum - amount;
     }
 
+    payable(meta.currentOwner).call{ value: (sum - fee) }("");
+    payable(feeAddress).call{ value: fee }("");
+    ERC721(meta.collectionAddress).safeTransferFrom(
+      address(this),
+      msg.sender,
+      meta.tokenId
+    );
+  }
 
-// Change in BuyNFT LibMeta Function
+  function sellNFT(
+    address _collectionAddress,
+    uint256 _tokenId,
+    uint256 _price
+  ) public onlyOwnerOfToken(_collectionAddress, _tokenId) nonReentrant {
+    _tokenIdTracker.increment();
 
-    function BuyNFT(uint256 _saleId) public payable nonReentrant {
-        LibMeta.TokenMeta memory meta = _tokenMeta[_saleId];
-        
-        LibShare.Share[] memory royalties;
+    //needs approval on frontend
+    ERC721(_collectionAddress).safeTransferFrom(
+      msg.sender,
+      address(this),
+      _tokenId
+    );
 
-        if(_tokenMeta[_saleId].collectionAddress == PNDCAddress) {
-            royalties = PNDC_ERC721(PNDCAddress).getRoyalties(_tokenMeta[_saleId].tokenId);
-        }
+    LibMeta.TokenMeta memory meta = LibMeta.TokenMeta(
+      _tokenIdTracker.current(),
+      _collectionAddress,
+      _tokenId,
+      _price,
+      true,
+      false,
+      true,
+      0,
+      0,
+      _msgSender()
+    );
 
-        else {
-            royalties = TokenERC721(_tokenMeta[_saleId].collectionAddress).getRoyalties(_tokenMeta[_saleId].tokenId);
-        }
+    _tokenMeta[_tokenIdTracker.current()] = meta;
 
-        require(meta.status);
-        require(msg.sender != address(0) && msg.sender != meta.currentOwner);
-        require(!meta.bidSale);
-        require(msg.value >= meta.price);
+    emit TokenMetaReturn(meta, _tokenIdTracker.current());
+  }
 
-        LibMeta.TokenMeta memory tok = LibMeta.transfer(_tokenMeta[_saleId],msg.sender);
-        _tokenMeta[_saleId] = tok;
+  function cancelSale(uint256 _saleId) public nonReentrant {
+    require(msg.sender == _tokenMeta[_saleId].currentOwner);
+    require(_tokenMeta[_saleId].status == true);
 
-        uint256 sum = msg.value;
-        uint256 val = msg.value;
-        uint256 fee = msg.value / 100;
-
-        for(uint256 i = 0; i < royalties.length; i ++) {
-            uint256 amount = (royalties[i].value * val) / 10000;
-            address payable receiver = royalties[i].account;
-            receiver.transfer(amount);
-            sum = sum - amount;
-        }
-
-        payable(meta.currentOwner).transfer(sum - fee);
-        payable(feeAddress).transfer(fee);
-        ERC721(meta.collectionAddress).safeTransferFrom(address(this), msg.sender, meta.tokenId);
-
-    }
-
-    function sellNFT(address _collectionAddress, uint256 _tokenId, uint256 _price) 
-    public 
-    onlyOwnerOfToken(_collectionAddress, _tokenId)
-    nonReentrant
-    {
-        _tokenIdTracker.increment();
-
-        //needs approval on frontend
-        ERC721(_collectionAddress).safeTransferFrom(msg.sender, address(this), _tokenId);
-
-        LibMeta.TokenMeta memory meta = LibMeta.TokenMeta(
-            _tokenIdTracker.current(),
-            _collectionAddress,
-            _tokenId,
-            _price,
-            true,
-            false,
-            true,
-            0,
-            0,
-            _msgSender()
-        );
-
-         _tokenMeta[_tokenIdTracker.current()] = meta;
-
-        emit TokenMetaReturn(meta, _tokenIdTracker.current());
-
-    }
-
-    function cancelSale(uint256 _saleId) public nonReentrant{
-
-        require(msg.sender == _tokenMeta[_saleId].currentOwner);
-        require(_tokenMeta[_saleId].status == true);
-
-        _tokenMeta[_saleId].status = false;
-        ERC721(_tokenMeta[_saleId].collectionAddress)
-        .safeTransferFrom(
-            address(this), 
-            _tokenMeta[_saleId].currentOwner, 
-            _tokenMeta[_saleId].tokenId
-            );
-
-    }
+    _tokenMeta[_saleId].status = false;
+    ERC721(_tokenMeta[_saleId].collectionAddress).safeTransferFrom(
+      address(this),
+      _tokenMeta[_saleId].currentOwner,
+      _tokenMeta[_saleId].tokenId
+    );
+  }
 }
