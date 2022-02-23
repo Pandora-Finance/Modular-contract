b373d4c1aaa64a1416fddf2d41e1a8fc9fbcc24c
sanskar khare <sanskarkhare@sanskars-MacBook-Air.local>
2022-02-23 15:45:31 +0530

    reduced some contract size by using already initialized variable

--- contracts/TokenERC1155.sol
+++ contracts/TokenERC1155.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.2;
+pragma solidity ^0.8.0;
 
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 import "@openzeppelin/contracts/access/Ownable.sol";
